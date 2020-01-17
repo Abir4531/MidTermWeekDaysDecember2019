de0d307107adb2fa1cd9058cd45e270b44dbed47
Abir4531 <abir_akib016@yahoo.com>
2020-01-17 01:30:49 -0500

    3rd commit

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,10 +1,34 @@
 package string.problems;
 
 
-public class Anagram {
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-        //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
-        //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+public class Anagram {
+    //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
+    //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+    public static boolean doAnagram(String a, String b){
+
+        boolean status=true;
+
+        if (a.length()!=b.length()){
+            status=false;
+        }
+        else{
+            char[] ArrayS1 =a.toLowerCase().toCharArray();
+            char[] ArrayS2 =b.toLowerCase().toCharArray();
+
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            status = Arrays.equals(ArrayS1,ArrayS2);
+        }
+        System.out.println(a+" "+b+" "+status);
+        return status;
     }
+    public static void main(String[] args) {
+
+        doAnagram("CAT","TAC");
+        doAnagram("ARMY","MARY");
+
+   }
 }
--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,7 +1,7 @@
 package string.problems;
 
 
-public class DuplicateWord {
+public class   DuplicateWord {
 
     public static void main(String[] args) {
         /*
